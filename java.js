34a26e19e029f0d6edf04d64d00b32d97ea8f72f
Siajiza <siajiza@gmail.com>
2021-04-05 18:48:41 +0200

    Cambios con Michelle

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 const navHamburger = document.querySelector('#navHamburger');
-
-    navHamburger.addEventListener('click', () => {
-
+    navHamburger.addEventListener('click', (e) => {
         navHamburger.parentElement.classList.toggle('active');
-})
+    })
+
+
