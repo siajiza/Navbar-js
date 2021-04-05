eba13bf5582d67ca4b4e37fac2d32eea979ed386
Siajiza <siajiza@gmail.com>
2021-04-05 12:37:36 +0200

    copia contenido de codigo inicial del proyecto

---
+++ java.js
@@ -0,0 +1,6 @@
+const navHamburger = document.querySelector('#navHamburger');
+
+    navHamburger.addEventListener('click', (e) => {
+
+        navHamburger.parentElement.classList.toggle('active');
+})
